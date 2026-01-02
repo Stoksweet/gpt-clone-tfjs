99f06bc2ce659a6e5a73c6c6ffa5243b8db51f98
Gabriel Ubuntu <smallycassim@gmail.com>
2026-01-02 23:35:08 +0200

    Added full gpt

---
+++ playground-node/src/index.ts
@@ -0,0 +1,65 @@
+import * as tf from '@tensorflow/tfjs-node-gpu'
+import { CONFIG, GPT, CharDataset, Trainer } from '@gpt/model'
+
+async function start() {
+  const backend = tf.getBackend()
+  console.log(`Current backend: ${backend}`)
+
+  const textSourceURL = 'https://raw.githubusercontent.com/trekhleb/homemade-gpt-js/refs/heads/main/playground-web/public/dataset-tinyshakespeare.txt'
+  const dataset = await CharDataset({ textSourceURL })
+
+  const batchSize = 96
+  const blockSize = 96
+  const maxIters = 21600
+  const evalInterval = 50
+  const evalIterations = 50
+  const learningRate = 1e-3
+
+  const model = GPT({
+    ...CONFIG['openai-gpt'],
+    blockSize,
+    vocabSize: dataset.vocabSize,
+  })
+  console.log('\nModel summary:', model.summary())
+
+  console.log('\nStart training:')
+  const trainer = Trainer({
+    model,
+    dataset,
+    params: {
+      learningRate,
+      evalInterval,
+      evalIterations,
+      maxIters,
+      batchSize,
+      blockSize,
+    },
+    callbacks: {
+      onEval: (params) => {
+        console.log(params)
+      },
+    },
+  })
+  await trainer.train()
+
+  console.log('\nStart generation:')
+  const generated = await model.generate({
+    idx: tf.ones([1, 1], 'int32'),
+    maxNewTokens: 500,
+    doSample: true,
+    topK: undefined,
+  })
+  console.log(dataset.decode(((await generated.array()) as number[][])[0]))
+
+  console.log('\nDisposing the model and dataset')
+  dataset.dispose()
+  generated.dispose()
+  model?.dispose?.()
+
+  console.log('Model Summary: ', model.summary());
+
+  console.log('\nDebug memory consumption:')
+  console.table(tf.memory())
+}
+
+start()
